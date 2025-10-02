a58009048779f747507f6968c3758bf3462bde19
scxrrow <120047758+scxrrow@users.noreply.github.com>
2025-10-02 12:24:04 +0200

    Update boucles.c

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -1,22 +1,19 @@
 #include <stdio.h>
 
 int main(){
-    int i=9;
-    for(int j=1; j<i; j++){
-        for(int k=1; k<=j; k++){
-            if(k==j || k==1 || j==i-1){
-                printf("* ");
+    int i = 9;
+    int j = 1;
+    while (j < i) {
+        int k = 1;
+        while (k <= j) {
+            if (k == j || k == 1 || j == i - 1) {
+                printf("*");
+            } else {
+                printf("#");
             }
-            else{
-                printf("# ");
-            }
-            
+            k++;
         }
         printf("\n");
+        j++;
     }
-    return 0;
 }
-
-
-
-
