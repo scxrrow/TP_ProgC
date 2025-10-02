4a38b6d5279f966c4cb8627f8ae8fe9c19655771
scxrrow <120047758+scxrrow@users.noreply.github.com>
2025-10-02 12:44:13 +0200

    Update conditions.c

---
+++ TP1/src/conditions.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+    int somme=0;
+    int tmp =0;
+
+    for(int i=1; i<1000; i++){
+        if(i%7==0 || i%5==0 && i%11!=0){
+            tmp= somme;
+            tmp +=i;
+            if(tmp >= 5000){
+                break;
+            }
+            else{
+                somme +=i;
+            }
+           
+        }
+        else if(i%11==0){
+            continue;
+        }
+    }
+    printf( "la somme est : %d", somme);
+
+}
