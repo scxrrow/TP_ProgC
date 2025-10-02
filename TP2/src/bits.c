8878916ba16f19a5bdb3fb9a2d97645202c3cc7a
scxrrow <120047758+scxrrow@users.noreply.github.com>
2025-10-02 16:03:53 +0200

    Update bits.c

---
+++ TP2/src/bits.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    unsigned int d = 0x00F00010; // Exemple de valeur, à modifier pour tester
+
+    int bits = sizeof(d) * 8; // Nombre total de bits (souvent 32)
+    int bit4 = (d >> (bits - 4)) & 1;    // 4ème bit de gauche
+    int bit20 = (d >> (bits - 20)) & 1;  // 20ème bit de gauche
+
+    if (bit4 == 1 && bit20 == 1){
+        printf("1\n");
+    }
+    else{
+        printf("0\n");
+    }
+}
