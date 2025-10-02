3cede98901409875cc69bb497c67005fd8ca33de
scxrrow <120047758+scxrrow@users.noreply.github.com>
2025-10-02 13:00:02 +0200

    Update calculs.c

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,6 +1,48 @@
+#include <stdio.h>
 
+int main(){
+    int num1 = 10;
+    int num2 = 10;
+    char op = '+';
 
-
-
-
-
+    switch (op) {
+        case '+':
+            printf("Result: %d\n", num1 + num2);
+            break;
+        case '-':
+            printf("Result: %d\n", num1 - num2);
+            break;
+        case '*':
+            printf("Result: %d\n", num1 * num2);
+            break;
+        case '/':
+            if (num2 != 0) {
+                printf("Result: %d\n", num1 / num2);
+            } else {
+                printf("Error: Division by zero\n");
+            }
+            break;
+        case '%':
+            if (num2 != 0) {
+                printf("Result: %d\n", num1 % num2);
+            } else {
+                printf("Error: Division by zero\n");
+            }
+            break;
+        case '||':
+            printf("Result: %d\n", num1 || num2);
+            break;
+        
+        case '&&':
+            printf("Result: %d\n", num1 && num2);
+            break;
+        
+        case '~':
+            printf("Result: %d\n", ~num1);
+            break;
+        
+        default:
+            printf("Error: Unknown operator\n");
+            break;
+    }
+}
