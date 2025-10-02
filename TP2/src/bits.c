a6951971f4181db35fd421597d0fbd3068f2e2f4
scxrrow <120047758+scxrrow@users.noreply.github.com>
2025-10-02 16:06:22 +0200

    Update bits.c

--- TP2/src/bits.c
+++ TP2/src/bits.c
@@ -3,7 +3,7 @@
 int main() {
     unsigned int d = 4096; // Exemple de valeur, à modifier pour tester
 
-    int bits = sizeof(d) * 8; // Nombre total de bits (souvent 32)
+    int bits = sizeof(d) * 8; // Nombre total de bits 
     int bit4 = (d >> (bits - 4)) & 1;    // 4ème bit de gauche
     int bit20 = (d >> (bits - 20)) & 1;  // 20ème bit de gauche
 
@@ -14,4 +14,5 @@ int main() {
         printf("0\n");
     }
 }
+
 
