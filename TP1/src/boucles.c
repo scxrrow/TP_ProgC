0f16a4d9001298f4f1b405a8a5b1d9b02cb02783
scxrrow <120047758+scxrrow@users.noreply.github.com>
2025-10-02 12:21:07 +0200

    Update boucles.c

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -4,7 +4,7 @@ int main(){
     int i=9;
     for(int j=1; j<i; j++){
         for(int k=1; k<=j; k++){
-            if(k==j || k==1 || k==i-1){
+            if(k==j || k==1 || j==i-1){
                 printf("* ");
             }
             else{
@@ -16,6 +16,7 @@ int main(){
     }
     return 0;
 }
+
 
 
 
