1f62d0db4ab576b801ae254689e4ef4482410d7a
scxrrow <120047758+scxrrow@users.noreply.github.com>
2025-10-02 15:22:59 +0200

    Update pyramide.c

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+int main() {
+    int n = 5;
+
+    for (int i = 1; i <= n; i++) {
+        // Espaces pour centrer
+        for (int j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+        // Nombres croissants
+        for (int j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+        // Nombres décroissants
+        for (int j = i - 1; j >= 1; j--) {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+
+    printf("Génération de la pyramide terminée.\n");
+}
