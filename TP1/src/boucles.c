cbe633f1b42124a293c480605d42e6dde2b372e0
scxrrow <120047758+scxrrow@users.noreply.github.com>
2025-10-02 12:12:29 +0200

    Update boucles.c

---
+++ TP1/src/boucles.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int i=9;
+    for(int j=1; j<i; j++){
+        for(int k=1; k<=j; k++){
+            if(k==j || k==1){
+                printf("*");
+            }
+            else{
+                printf("#");
+            }
+            
+        }
+        printf("\n");
+    }
+}
