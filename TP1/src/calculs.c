18b2f8cdf0855f39bfc1236c8f629f7626f6fc25
scxrrow <120047758+scxrrow@users.noreply.github.com>
2025-10-02 12:31:35 +0200

    Update calculs.c

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int somme=0;
+
+    for(int i=1; i<1000; i++){
+        if(i%3==0 || i%5==0 && i%11!=0){
+            somme+=i;
+        }
+        else if(i%11==0){
+            continue;
+        }
+        else if(somme >= 5000){
+            break;
+        }
+    }
+}
 
