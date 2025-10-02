070728061ee9eda505086eb057b8be189e194b7d
scxrrow <120047758+scxrrow@users.noreply.github.com>
2025-10-02 15:51:18 +0200

    Update puissance.c

--- TP2/src/puissance.c
+++ TP2/src/puissance.c
@@ -3,10 +3,12 @@
 int main(){
   int b = 3;
   int a= 2;
+  int puissance = a;
   for(int i= 1; i<b ; i++){
-    a=a*a;
+    puissance = puissance * a;
   }
   printf("a puissance b = %d", a);
   
 }
+
 
