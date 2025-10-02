17d564fc472a9c39c5cfac26ab0dbe7433c7c3fa
scxrrow <120047758+scxrrow@users.noreply.github.com>
2025-10-02 12:42:03 +0200

    Update calculs.c

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -2,21 +2,28 @@
 
 int main(){
     int somme=0;
+    int tmp =0;
 
     for(int i=1; i<1000; i++){
         if(i%7==0 || i%5==0 && i%11!=0){
-            somme+=i;
+            tmp= somme;
+            tmp +=i;
+            if(tmp >= 5000){
+                break;
+            }
+            else{
+                somme +=i;
+            }
+           
         }
         else if(i%11==0){
             continue;
-        }
-        else if(somme >= 5000){
-            break;
         }
     }
     printf( "la somme est : %d", somme);
 
 }
+
 
 
 
