9471174ea1e11589c9be6a12d1c3b640977f130e
scxrrow <120047758+scxrrow@users.noreply.github.com>
2025-10-02 11:59:17 +0200

    Update opérateurs.c

---
+++ TP1/src/opérateurs.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int a = 16;
+    int b = 3;
+
+    // Addition
+    printf("a + b = %d\n", a + b);
+
+    // soustraction
+    printf("a - b = %d\n", a - b);
+
+    // Multiplication
+    printf("a * b = %d\n", a * b);
+
+    // Division
+    printf("a / b = %d\n", a / b);
+
+    // Modulo
+    printf("a %% b = %d\n", a % b);
+
+    // comparaison égalité
+    printf("a == b: %d\n", a == b);
+
+    // Comparaison plus grand que
+    printf("a > b: %d\n", a > b);
+}
