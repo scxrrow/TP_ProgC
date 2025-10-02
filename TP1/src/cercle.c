31763af7334f87776dfbc6f632e3ab1887cc88f4
scxrrow <120047758+scxrrow@users.noreply.github.com>
2025-10-02 11:36:38 +0200

    Update cercle.c

---
+++ TP1/src/cercle.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    double pi = 3.14159;
+    double r = 6.0;
+
+    printf("l'air du cercle est : %f", pi * r * r);
+    printf("le perimetre du cercle est : %f", 2 * pi * r);
+}
